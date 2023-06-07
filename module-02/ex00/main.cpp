761cea5cc9d693baea8bbfa6103659813971e5b0
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-06-07 21:23:06 +0000

    feat: add more test to module-02 ex00

--- module-02/ex00/main.cpp
+++ module-02/ex00/main.cpp
@@ -12,5 +12,10 @@ int main( void )
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
+	a.setRawBits(10);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
 	return 0;
 }
