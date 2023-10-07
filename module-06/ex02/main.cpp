b718586428c0080d8ccf2b8bf6fdfb2af0d4f790
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-10-07 18:01:23 +0200

    test: making more explicit test for module-06 ex02

--- module-06/ex02/main.cpp
+++ module-06/ex02/main.cpp
@@ -13,17 +13,17 @@ Base *generate(void)
 	{
 	case 0:
 		ptr = new A();
-		std::cout << "Creating A\n";
+		std::cout << "Creating A : ";
 		break;
 
 	case 1:
 		ptr = new B();
-		std::cout << "Creating B\n";
+		std::cout << "Creating B : ";
 		break;
 
 	case 2:
 		ptr = new C();
-		std::cout << "Creating C\n";
+		std::cout << "Creating C : ";
 		break;
 
 	default:
@@ -49,6 +49,7 @@ void identify(Base &p)
 		A& base_A = dynamic_cast<A&>(p);
 		(void)base_A;
 		std::cout << "Base is of type A&" << std::endl;
+		return ;
 	}
 	catch(const std::exception& e)
 	{
@@ -59,6 +60,7 @@ void identify(Base &p)
 		B& base_B = dynamic_cast<B&>(p);
 		(void)base_B;
 		std::cout << "Base is of type B&" << std::endl;
+		return ;
 	}
 	catch(const std::exception& e)
 	{
@@ -69,6 +71,7 @@ void identify(Base &p)
 		C& base_C = dynamic_cast<C&>(p);
 		(void)base_C;
 		std::cout << "Base is of type C&" << std::endl;
+		return ;
 	}
 	catch(const std::exception& e)
 	{
@@ -77,6 +80,7 @@ void identify(Base &p)
 
 int main()
 {
+	std::cout << "-- Testing pointers --\n\n";
 	for (int i = 0; i != 10; i++)
 	{
 		Base* new_base = generate();
@@ -84,10 +88,14 @@ int main()
 		delete new_base;
 	}
 
+	std::cout << "\n-- Testing reference --\n\n";
+	std::cout << "Creating object A\n";
 	A base_A;
-	B base_B;
-	C base_C;
 	identify(base_A);
+	std::cout << "Creating object B\n";
+	B base_B;
 	identify(base_B);
+	std::cout << "Creating object C\n";
+	C base_C;
 	identify(base_C);
 }
