c895dd573adfd6ba88be8b99b1e166af499f3242
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-11-25 23:07:33 +0000

    fix: now correctly sort numbers

--- module-09/ex02/TPmergeMe.hpp
+++ module-09/ex02/TPmergeMe.hpp
@@ -1,129 +1,133 @@
 #pragma once
 
-#include <list>
-#include <vector>
-#include <sstream>
-#include <iostream>
-#include <cmath>
-#include <iterator>
 #include <algorithm>
+#include <cmath>
 #include <cstring>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <sstream>
+#include <vector>
 
-typedef unsigned int value_type;
+typedef unsigned int valueType;
 
-template <typename T = std::list<value_type> >
-class TPmergeMe
-{
-	public :
+template <typename T = std::list<valueType> >
+class TPmergeMe {
+   public:
 	TPmergeMe() {}
-	TPmergeMe(const T & value) : _values(value) {}
-	TPmergeMe(const TPmergeMe & copy) {*this = copy;}
+	TPmergeMe(const T &value) : _values(value) {}
+	TPmergeMe(const TPmergeMe &copy) { *this = copy; }
 	~TPmergeMe() {}
-	TPmergeMe & operator=(const TPmergeMe & assign)
-	{
-		if (this != &assign)
-		{
+	TPmergeMe &operator=(const TPmergeMe &assign) {
+		if (this != &assign) {
 			_values = assign._values;
 		}
 		return *this;
 	}
 
-	T sort()
-	{
-		T values = sort_list(_values);
+	T sort() {
+		T values = sortList(_values);
 		_values = values;
 		return (values);
 	}
-    T get_values() const {return _values;}
+	T getValues() const { return _values; }
 
-	private :
+   private:
 	T _values;
 
-    std::pair<T, T> create_pairs(const T & values)
-	{
-		T first_list;
-		T second_list;
-		for (typename T::const_iterator it = values.begin(); it != values.end(); std::advance(it, 2))
-		{
-			typename T::const_iterator it_for = it;
-			typename T::value_type first_value = *it_for;
-			it_for++;
-			if (it_for == values.end())
-				throw std::runtime_error("Error: creating pair with odd numbers.");
-			typename T::value_type second_value = *it_for;
-			if (first_value < second_value)
-				std::swap(first_value, second_value);
-			first_list.push_back(first_value);
-			second_list.push_back(second_value);
+	std::pair<T, T> createPairs(const T &values) {
+		T firstList;
+		T secondList;
+		for (typename T::const_iterator it = values.begin(); it != values.end(); std::advance(it, 2)) {
+			typename T::const_iterator itFor = it;
+			typename T::value_type firstValue = *itFor;
+			itFor++;
+			if (itFor == values.end()) throw std::runtime_error("Error: creating pair with odd numbers.");
+			typename T::value_type secondValue = *itFor;
+			if (firstValue < secondValue) std::swap(firstValue, secondValue);
+			firstList.push_back(firstValue);
+			secondList.push_back(secondValue);
 		}
-		return std::make_pair(first_list, second_list);
+		return std::make_pair(firstList, secondList);
 	}
 
-    void binary_sort(T &main_list, T &pend_list)
-	{
-		for (typename T::iterator it = pend_list.begin(); it != pend_list.end(); ++it)
-		{
-			typename T::iterator pos = binary_search(main_list, *it);
-			main_list.insert(pos, *it);
-		}
+	void insert(T &mainList, T &pendList, size_t advance) {
+		typename T::iterator it = pendList.begin();
+		std::advance(it, advance);
+		typename T::value_type value = *it;
+		typename T::iterator pos = binarySearch(mainList, value);
+		mainList.insert(pos, value);
 	}
 
-	unsigned int jacobsthal(unsigned int n) {
-		if (n == 0)
-			return 0;
-		else if (n == 1)
-			return 1;
-		else
-			return jacobsthal(n-1) + 2 * jacobsthal(n-2);
-	}
+	void binarySort(T &mainList, T &pendList) {
+		static unsigned int jacobsthalArray[] = {0, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461, 10923, 21845, 43691,
+												 87381, 174763, 349525, 699051, 1398101, 2796203, 5592405, 11184811, 22369621, 44739243, 89478485, 178956971, 357913941, 715827883, 1431655765, 2863311531};
 
-	typename T::iterator binary_search(T& list, unsigned int key)
-	{
-		static unsigned int jacob[] = {0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341, 683, 1365, 2731, 5461, 10923, 21845, 43691, 87381, 174763, 349525};
-		size_t low = 0;
-		size_t high = list.size();
-		unsigned int mid;
-		typename T::iterator it_mid = list.begin();
-		while (low < high)
-		{
-			for (int i = 0; jacob[i] <= high - low - 1; i++)
-			{
-				mid = jacob[i];
+		size_t pendSize = pendList.size();
+		for (size_t i = 0; i < sizeof(jacobsthalArray) / sizeof(*jacobsthalArray) && i < pendSize; i++) {
+			unsigned int jacobsthalNumber = jacobsthalArray[i];
+			if (jacobsthalNumber <= 1) {
+				insert(mainList, pendList, jacobsthalNumber);
+				continue;
+			} else if (jacobsthalNumber > pendSize - 1) {
+				jacobsthalNumber = pendSize - 1;
 			}
-			it_mid = list.begin();
-			std::advance(it_mid, mid);
-			if(*it_mid < key)
-				low = mid + 1;
-			else
-				high = mid;
+			for (size_t pendIndex = jacobsthalNumber; pendIndex > jacobsthalArray[i - 1]; pendIndex--) {
+				insert(mainList, pendList, pendIndex);
+			}
 		}
-		return it_mid;
+		if (pendSize - 1 > jacobsthalArray[sizeof(jacobsthalArray) / sizeof(*jacobsthalArray) - 1]) {
+			for (size_t pendIndex = pendSize - 1; pendIndex > jacobsthalArray[sizeof(jacobsthalArray) / sizeof(*jacobsthalArray) - 1]; pendIndex--) {
+				insert(mainList, pendList, pendIndex);
+			}
+		}
 	}
 
-	T sort_list(T value)
-	{
-		if (value.size() <= 1)
-			return value;
+	typename T::iterator binarySearch(T &list, unsigned int key) {
+		typename T::iterator start = list.begin();
+		typename T::iterator end = list.end();
+		while (std::distance(list.begin(), start) < std::distance(list.begin(), end)) {
+			typename T::iterator mid = start;
+			std::advance(mid, (std::distance(start, end) / 2));
+			if (*mid > key) {
+				if (mid == list.begin())
+					return mid;
+				else {
+					typename T::iterator prev = mid;
+					prev--;
+					if (*prev < key) return mid;
+				}
+			}
+			if (*mid > key) {
+				end = mid;
+			} else {
+				start = mid;
+				start++;
+			}
+		}
+		return list.end();
+	}
+
+	T sortList(T value) {
+		if (value.size() <= 1) return value;
 		bool is_even = true;
 		typename T::value_type straggler;
-		if (value.size() % 2 != 0)
-		{
+		if (value.size() % 2 != 0) {
 			is_even = false;
 			straggler = value.back();
 			value.pop_back();
 		}
 
-		std::pair<T, T> pairs = create_pairs(value);
+		std::pair<T, T> pairs = createPairs(value);
 		T main_list = pairs.first;
 		T pend_list = pairs.second;
-		main_list = sort_list(main_list);
+		main_list = sortList(main_list);
 
-		binary_sort(main_list, pend_list);
-		if (!is_even)
-		{
+		binarySort(main_list, pend_list);
+		if (!is_even) {
 			T straggler_list;
 			straggler_list.push_back(straggler);
-			binary_sort(main_list, straggler_list);
+			binarySort(main_list, straggler_list);
 		}
 		return (main_list);
 	}
